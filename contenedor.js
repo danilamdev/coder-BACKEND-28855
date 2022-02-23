579926bab24c5cf6c58b66ff71d92b8c946ca6ef
daniel lambarri <dalambarri@live.com>
2022-02-22 23:00:28 -0300

    clase 02 end

---
+++ contenedor.js
@@ -0,0 +1,62 @@
+const fs = require('fs')
+
+class Contenedor {
+   constructor(fileName) {
+      this.fileName = fileName
+   }
+
+   async save(newProduct) {
+      try {
+         const file = JSON.parse(await fs.promises.readFile(this.fileName))
+         const ids = file.map(f => f.id)
+         const newID = Math.max(...ids) + 1
+
+         newProduct = { id: newID, ...newProduct }
+         file.push(newProduct)
+
+         await fs.promises.writeFile(this.fileName, JSON.stringify(file, null, 2))
+
+         return newID
+
+      } catch (error) {
+         return Promise.reject({ Error: 'hubo un error al leer el archivo...' })
+      }
+   }
+
+   async getById(id) {
+      const file = JSON.parse(await fs.promises.readFile(this.fileName))
+      const fileById = await file.find(f => f.id === id)
+
+      if (typeof fileById === 'undefined') return { Error: 'no se encontro nada' }
+
+      return fileById
+   }
+
+   async getAll() {
+      try {
+         const products = JSON.parse(await fs.promises.readFile(this.fileName))
+         return products
+
+      } catch (error) {
+         return Promise.reject('Error: No se encontro el archivo')
+      }
+   }
+
+   async deleteById(id) {
+      const file = JSON.parse(await fs.promises.readFile(this.fileName))
+      const newFile = file.filter(f => f.id !== id)
+      await fs.promises.writeFile(this.fileName, JSON.stringify(newFile, null, 2))
+
+      return `producto con id ${id} eliminado`
+
+   }
+
+   async deleteAll() {
+      await fs.promises.writeFile(this.fileName, '')
+      return 'eliminado todo el contenido'
+   }
+}
+
+
+module.exports = new Contenedor('products.json')
+
